3fb3debfbe03b932ac7e006b3d26626c73e037fe
Alize <alizewulf@gmail.com>
2026-06-06 11:36:59 +0400

    0.10.0

--- src/services/API_CONFIG.ts
+++
@@ -1,2 +0,0 @@
-export const API_USERS_URL = "https://6a1f31efb79eec0d6cf09493.mockapi.io/exclusive/users"
-export const API_PRODUCTS_URL = "https://6a1f31efb79eec0d6cf09493.mockapi.io/exclusive/products"
